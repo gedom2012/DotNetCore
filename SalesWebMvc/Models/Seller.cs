850efa8d87217051325dbbfdcd976c53b805fe8c
Marcelo Mariath <gedom2012@gmail.com>
2021-10-19 00:40:53 +0100

    First Controller - Department

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Salary { get; set; }
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double salary)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Salary = salary;
+        }
+    }
+}
