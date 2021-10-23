82290bc9bdc73112340c8375f4c62e5145ee725d
Marcelo Mariath <gedom2012@gmail.com>
2021-10-23 09:33:54 +0100

    Revert "CRUD Scaffolding"
    This reverts commit 7c7f49e0c0afdc022f8525781fb8af41fbd01060.

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -9,8 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -35,9 +33,6 @@ namespace SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
