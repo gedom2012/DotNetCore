8727d25b3ea8d9cc016d07a512dc3398d00804c4
gedom2012 <gedom2012@gmail.com>
2021-11-09 22:36:27 +0000

    Sellers Controller

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller // ao carregarmos no link chamamos este controlador
+    {
+        public IActionResult Index() //o método retorna a view com base no folder com nome Sellers e busca o Index.cshtml nesta pasta
+        {
+            return View();
+        }
+    }
+}
